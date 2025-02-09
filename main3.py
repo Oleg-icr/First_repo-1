780bdf8524f1bdf1036972638c9b819d3f90f18a
Oleg-Icr <oleg.icr@gmail.com>
2025-02-09 17:23:02 +0100

    Factorial practice

--- main3.py
+++ main3.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 # Створення об'єкта datetime
 now = datetime.now()
@@ -18,8 +18,6 @@ print("UTC now       :" , utc_now)  # Виведе поточний час в UT
 
 print("===================================================")
 
-from datetime import datetime, timezone, timedelta
-
 utc_time = datetime.now(timezone.utc)
 print (utc_time)
 
@@ -28,3 +26,53 @@ eastern_time = utc_time.astimezone(timezone(timedelta(hours=-5)))
 # Перетворює час UTC в час Східного часового поясу
 print(eastern_time)
 
+print("===================================================")
+
+# Час у конкретній часовій зоні
+timezone_offset = timezone(timedelta(hours=2))  # Наприклад, UTC+2
+timezone_datetime = datetime(year=2023, month=3, day=14, hour=12, minute=30, second=0, tzinfo=timezone_offset)
+print (timezone_offset)
+print (timezone_datetime)
+# Конвертація у формат ISO 8601
+iso_format_with_timezone = timezone_datetime.isoformat()
+print(iso_format_with_timezone)
+
+print("===================================================")
+
+import time
+
+current_time = time.time()
+print(f"Поточний час: {current_time}")
+
+print("===================================================")
+
+current_time = time.time()
+print(f"Поточний час: {current_time}")
+
+readable_time = time.ctime(current_time)
+print(f"Читабельний час: {readable_time}")
+
+print("===================================================")
+
+current_time = time.time()
+print(f"Поточний час: {current_time}")
+
+local_time = time.localtime(current_time)
+print(f"Місцевий час: {local_time}")
+
+print("===================================================")
+
+# Записуємо час на початку виконання
+start_time = time.perf_counter()
+print (start_time)
+
+# Виконуємо якусь операцію
+for _ in range(1000000):
+    pass  # Просто проходить цикл мільйон разів
+
+# Записуємо час після виконання операції
+end_time = time.perf_counter()
+print (end_time)
+# Розраховуємо та виводимо час виконання
+execution_time = end_time - start_time
+print(f"Час виконання: {execution_time} секунд")
