df865bb5d6b7054b18879f4345b51a0f05c84ff0
Oleg-Icr <oleg.icr@gmail.com>
2025-02-19 22:03:13 +0100

    Date time - home work - autocheck - part 2

--- date_tim_homework_auto.py
+++ date_tim_homework_auto.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-
+print ("============part 1 ========================")
 def string_to_date(date_string):
     parced_date = datetime.strptime(date_string, "%Y.%m.%d").date()
     return parced_date
@@ -11,3 +11,24 @@ def date_to_string(date):
 
 
 print (string_to_date("2020.01.01"))
+
+print ("============part 2 ========================")
+
+def prepare_user_list(user_data):
+    for users in user_data:
+        users["birthday"] = datetime.strptime(users["birthday"], "%Y.%m.%d").date()
+
+    return user_data
+
+users = [
+    {"name": "Bill Gates", "birthday": "1955.3.25"},
+    {"name": "Steve Jobs", "birthday": "1955.3.21"},
+    {"name": "Jinny Lee", "birthday": "1956.3.22"},
+    {"name": "John Doe", "birthday": "1985.01.23"},
+    {"name": "Jane Smith", "birthday": "1990.01.27"}
+]
+
+prepared_users = prepare_user_list(users)
+print(prepared_users)
+
+
